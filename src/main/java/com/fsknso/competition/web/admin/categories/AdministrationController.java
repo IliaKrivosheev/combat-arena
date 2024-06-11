41470fcd1980a1f4e10437d0c1dd832fdf5563bb
Ilia Krivosheev <krivosheev@scaleapps.ru>
2024-06-11 16:50:48 +0700

    upd generating Category name

--- src/main/java/com/fsknso/competition/web/admin/categories/AdministrationController.java
+++ src/main/java/com/fsknso/competition/web/admin/categories/AdministrationController.java
@@ -6,11 +6,19 @@ import com.fsknso.competition.core.repository.categories.CategoryRepository;
 import com.fsknso.competition.core.repository.categories.WeightCategoryRepository;
 import com.fsknso.competition.web.common.BaseController;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
 
@@ -53,6 +61,34 @@ public class AdministrationController extends BaseController {
         model.addAttribute("weightCategories", weightCategories);
         model.addAttribute("grades", grades);
         model.addAttribute("genders", genders);
-        return "admin/categories/category/create-new-category";
+        return FOLDER_PATH + "create-new-category";
+    }
+
+    @PostMapping("/create-new-category")
+    public String createNewCategoryPage(Model model, @RequestBody String requestBody) {
+        log.info("Opening Categories Administration Page...");
+
+        log.info("Receiving POST request to create a new category...");
+
+        log.warn("requestBody: {}", requestBody);
+
+//        // Получаем URI запроса
+//        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().build().toUri();
+//
+//        // Создаем HttpHeaders и HttpEntity с телом запроса
+//        HttpHeaders headers = new HttpHeaders();
+//        HttpEntity<String> requestEntity = new HttpEntity<>(requestBody, headers);
+//
+//        // Используем RestTemplate для получения тела запроса
+//        RestTemplate restTemplate = new RestTemplate();
+//        String requestPayload = restTemplate.exchange(uri, HttpMethod.POST, requestEntity, String.class).getBody();
+//
+//        // Здесь requestPayload содержит тело POST запроса
+//        log.warn("Request payload: " + requestPayload);
+
+
+        List<Category> categories = categoryRepository.findAll();
+        model.addAttribute("categories", categories);
+        return "redirect:" + FOLDER_PATH + "administration";
     }
 }
